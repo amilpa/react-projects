ae6bba00f6dcfe57966ccb195f51d364f8614b0c
Amil P A <amilpa2020@gmail.com>
2023-02-24 17:30:50 +0530

    added search function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { useState } from 'react';
-import JSONDATA from './MOCK_DATA.json'
+
+
+import './App.css';
+
 
 
 function App() {
-  return(
-    <div className="App">
-      <input type="text" placeholder='Search...'/>
-    </div>
-  )
+	return(
+		<div>
+			hello world
+		</div>
+	)
 }
 
 export default App;
